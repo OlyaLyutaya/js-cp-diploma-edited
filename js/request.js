577e486de50fda83a90f83fd58eb0789742a962f
olyalyutaya <you@example.com>
2023-05-20 11:54:46 +0300

    переделала

--- js/request.js
+++ js/request.js
@@ -1,5 +1,40 @@
 "use strict";
 
+function createRequest(requestBodyString, requestSourceString = "", callback, uploadInfoIsNeed = false) {
+
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://jscp-diplom.netoserver.ru/");
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.send(requestBodyString);
+
+  if (uploadInfoIsNeed) {
+    xhr.upload.onprogress = function (event) {
+      console.log(`Отправка данных... Отправлено ${event.loaded} из ${event.total} байт`);
+    };
+
+    xhr.upload.onerror = function () {
+      console.log("Произошла ошибка при загрузке данных на сервер!");
+    };
+  }
+
+  xhr.onload = function () {
+    if (xhr.status != 200) {
+      alert("Ошибка: " + xhr.status);
+      return;
+    }
+
+    console.log(`${requestSourceString} - статус запроса: ${xhr.status} (${xhr.statusText})`);
+    callback(xhr.response);
+
+  };
+
+  xhr.onerror = function () {
+    alert("Запрос не удался");
+  };
+
+};
+
+
 function setItem(key, value) {
   try {
     return window.sessionStorage.setItem(key, value);
@@ -16,6 +51,7 @@ function getItem(key, value) {
   }
 }
 
+
 function setJSON(key, value) {
   try {
     const json = JSON.stringify(value); 
@@ -25,6 +61,7 @@ function setJSON(key, value) {
     console.error(e);
   }
 }
+
 
 function getJSON(key) {
   try {
